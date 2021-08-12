6a1cbe88df442e9010b656ac0de0488bee4fdc6e
Amneh-momani <Amneh99el@gmail.com>
2021-08-12 00:25:12 -0700

    delete n add

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Jumbotron } from "react-bootstrap";
 
-<<<<<<< HEAD:src/BestBooks.js
-class MyFavoriteBooks extends React.Component {
-=======
-
 
 
 class Welcome extends React.Component {
->>>>>>> 15bc63d74bf66fa6f27f1b1eec85c9d21e6b3fe1:src/Welcome.js
   render() {
     
     return (
---
+++ src/components/FormModal.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export class FormModal extends Component {
+  render() {
+    return (
+      <div>
+        <Modal show={this.props.show} onHide={this.props.handleDisplayModal}>
+          <Modal.Header>
+            <Modal.Title>Add Book Form</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={(e) => this.props.handleSubmitForm(e)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Book Name</Form.Label>
+                <Form.Control name="bookName" type="text" placeholder="Enter the book name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Book Description</Form.Label>
+                <Form.Control name="bookDes" type="text" placeholder="Enter the book Description" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Book Status</Form.Label>
+                <Form.Control name="bookStatus" type="text" placeholder="Enter the book Status" />
+              </Form.Group>
+              
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Image URL</Form.Label>
+                <Form.Control name="Image" type="text" placeholder="Enter the image URL" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Add Book
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleDisplayModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default FormModal
